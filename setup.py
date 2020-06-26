8d61d51e64621e681bf6ec54411b186cf10cafa4
Neal Beeken <nbbeeken@gmail.com>
2020-06-26 19:26:54 -0400

    wip

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
+"""Setup.py shim for installing dashmips in editable mode."""
 import setuptools
 
 # NOT MEANT FOR GENERAL CONSUMPTION
 # THIS IS ONLY FOR LOCAL DEVELOPMENT
 
-setuptools.setup(
-    name='dashmips',
-    version="INVALID"
-)
+setuptools.setup(name="dashmips", version="INVALID")
