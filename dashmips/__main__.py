c2d6d04ef5e8b5435778d5690071c858ce8e7565
Joshua Mitchener <jmitchen@uci.edu>
2020-07-31 19:05:04 -0700

    Formatting

--- dashmips/__main__.py
+++ dashmips/__main__.py
@@ -27,7 +27,7 @@ def main_compile(args: argparse.Namespace) -> int:
                 [
                     chr(int(num, 16))
                     for segments in findall(
-        f"({hex}{hex} {hex}{hex} {hex}{hex} {hex}{hex})    <....<\n", program.to_dict()["memory"][section].replace("|", "<")
+                f"({hex}{hex} {hex}{hex} {hex}{hex} {hex}{hex})    <....<\n", program.to_dict()["memory"][section].replace("|", "<")
                     )
                     for num in segments.split()
                 ]
